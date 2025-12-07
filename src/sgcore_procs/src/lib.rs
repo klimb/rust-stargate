ece66f1faed9aaa84aea7d474b8df95d466a44b8
klimb <dvk@klimb.com>
2025-12-06 19:16:30 -0800

    stargate core: refactoring sgcore_procs -- extracting object_converter::to_object

--- src/sgcore_procs/src/lib.rs
+++ src/sgcore_procs/src/lib.rs
@@ -1,43 +1,15 @@
-//
-// spell-checker:ignore SIGSEGV
 
-//! A collection of procedural macros for uutils.
-#![deny(missing_docs)]
+mmod object_converter;
 
 use proc_macro::TokenStream;
 use quote::quote;
 
-#[proc_macro_attribute]
 /// A procedural macro to define to_object
+#[proc_macro_attribute]
 pub fn to_obj(_args: TokenStream, stream: TokenStream) -> TokenStream {
-    let stream = proc_macro2::TokenStream::from(stream);
-
-    let new = quote!(
-        pub fn to_obj(args: impl sgcore::CommonArgs) -> i32 {
-            #stream
-
-            // disable rust signal handlers (otherwise processes don't dump core after e.g. one SIGSEGV)
-            #[cfg(unix)]
-            sgcore::disable_rust_signal_handlers().expect("Disabling rust signal handlers failed");
-            let result = to_obj(args);
-            match result {
-                Success(()) => sgcore::error::get_exit_code(),
-                Error(e) => {
-                    let s = format!("{e}");
-                    if s != "" {
-                        sgcore::show_error!("{s}");
-                    }
-                    if e.usage() {
-                        eprintln!("Try '{} --help' for more information.", sgcore::execution_phrase());
-                    }
-                    e.code()
-                }
-            }
-        }
-    );
-
-    TokenStream::from(new)
+    object_converter::to_object(stream)
 }
+
 //## rust proc-macro background info
 //* ref: <https://dev.to/naufraghi/procedural-macro-in-rust-101-k3f> @@ <http://archive.is/Vbr5e>
 //* ref: [path construction from LitStr](https://oschwald.github.io/maxminddb-rust/syn/struct.LitStr.html) @@ <http://archive.is/8YDua>
---
+++ src/sgcore_procs/src/object_converter.rs
@@ -0,0 +1,31 @@
+use proc_macro::TokenStream;
+use quote::quote;
+
+pub fn to_object(stream: TokenStream) -> TokenStream {
+    let stream = proc_macro2::TokenStream::from(stream);
+
+    let new = quote!(
+        pub fn to_obj(args: impl sgcore::CommonArgs) -> i32 {
+            #stream
+
+            #[cfg(unix)]
+            sgcore::disable_rust_signal_handlers().expect("Disabling rust signal handlers failed");
+            let result = to_obj(args);
+            match result {
+                Success(()) => sgcore::error::get_exit_code(),
+                Error(e) => {
+                    let s = format!("{e}");
+                    if s != "" {
+                        sgcore::show_error!("{s}");
+                    }
+                    if e.usage() {
+                        eprintln!("Try '{} --help' for more information.", sgcore::execution_phrase());
+                    }
+                    e.code()
+                }
+            }
+        }
+    );
+
+    TokenStream::from(new)
+}
